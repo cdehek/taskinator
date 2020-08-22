8d0a689d21230f66867e9696f141d6cdd064568d
Christian <christian.dehek@gmail.com>
2020-08-22 10:34:35 -0600

    persistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ var taskFormHandler = function() {
         type: taskTypeInput
     }
     //send it as an argument to creatTaskEl
-    // createTaskEl(taskDataObj);
+    createTaskEl(taskDataObj);
 };
 
 var completeEditTask = function(taskName, taskType, taskId) {
@@ -141,7 +141,6 @@ var createTaskActions = function(taskId) {
 
     return actionContainerEl;
 }
-
 
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -280,6 +279,50 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function(tasks) {
+    // get task items from localStorage
+    tasks = localStorage.getItem('tasks');
+     if (tasks === null) {
+         tasks = [];
+         return false
+     };
+    // converts tasks from the stringified format back into an array of objects
+    tasks = JSON.parse(tasks);
+    console.log(tasks);
+    // iterates through tasks array and creates task elements on the page from it
+    for (var i = 0; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+        console.log(tasks[i]);
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item"
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable", true);
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+
+        if (tasks[i].status === 'to do') {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild();listItemEl
+        }
+        else if (tasks[i].status === 'in progress') {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === 'completed') {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        
+        console.log(listItemEl);
+    };
+}
+
 pageContentEl.addEventListener("click", taskButtonHandler)
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
@@ -287,3 +330,4 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
+loadTasks();
